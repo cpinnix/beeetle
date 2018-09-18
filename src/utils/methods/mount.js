5fac5704b5587063a82ca4885f8b4dae4e440962
Charles Pinnix <cpinnix89@gmail.com>
2018-09-18 18:49:57 -0400

    Immutable.

--- src/utils/methods/mount.js
+++ src/utils/methods/mount.js
@@ -3,9 +3,15 @@ export const mount = _ => ({
   mount: base => state => {
     if (base.hooks && base.hooks.mount) {
       base.hooks.mount({
-        setProps: fn => base.setProps(base)(state)(fn),
-        setActions: fn => base.setActions(base)(state)(fn),
-        setAttributes: fn => base.setAttributes(base)(state)(fn)
+        setProps: fn => {
+          state = base.setProps(base)(state)(fn);
+        },
+        setActions: fn => {
+          state = base.setActions(base)(state)(fn);
+        },
+        setAttributes: fn => {
+          state = base.setAttributes(base)(state)(fn);
+        }
       });
     }
     base.update(base)(state);
