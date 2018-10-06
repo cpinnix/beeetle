437e8d327e62e534a67ea74c7496a197e6f8148a
Charles Pinnix <cpinnix89@gmail.com>
2018-10-06 11:34:33 -0400

    Removed Stupid Timeout

--- src/index.js
+++ src/index.js
@@ -2,24 +2,22 @@
 
 import { Router } from "@vaadin/router";
 
-setTimeout(() => {
-  const router = new Router(document.getElementById("root"));
+const router = new Router(document.getElementById("root"));
 
-  router.setRoutes([
-    {
-      path: "/",
-      onBeforeEnter: import(/* webpackChunkName: "vs-route-home" */ "./routes/vs-route-home"),
-      component: "vs-route-home"
-    },
-    {
-      path: "/clock",
-      onBeforeEnter: import(/* webpackChunkName: "vs-route-clock" */ "./routes/vs-route-clock"),
-      component: "vs-route-clock"
-    },
-    {
-      path: "/triangle",
-      onBeforeEnter: import(/* webpackChunkName: "vs-route-triangle" */ "./routes/vs-route-triangle"),
-      component: "vs-route-triangle"
-    }
-  ]);
-}, 1000);
+router.setRoutes([
+  {
+    path: "/",
+    onBeforeEnter: import(/* webpackChunkName: "vs-route-home" */ "./routes/vs-route-home"),
+    component: "vs-route-home"
+  },
+  {
+    path: "/clock",
+    onBeforeEnter: import(/* webpackChunkName: "vs-route-clock" */ "./routes/vs-route-clock"),
+    component: "vs-route-clock"
+  },
+  {
+    path: "/triangle",
+    onBeforeEnter: import(/* webpackChunkName: "vs-route-triangle" */ "./routes/vs-route-triangle"),
+    component: "vs-route-triangle"
+  }
+]);
