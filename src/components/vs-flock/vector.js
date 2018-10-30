9060d0193bee0533b75840085ae2f1305205dce5
Charles Pinnix <cpinnix89@gmail.com>
2018-10-30 07:13:31 -0400

    Vector Refactoring

--- src/components/vs-flock/vector.js
+++ src/components/vs-flock/vector.js
@@ -1,46 +1,65 @@
+const add = (p, ...rest) => {
+  let { x, y } = p;
+  for (let i = 0; i < rest.length; i++) {
+    x += rest[i].x;
+    y += rest[i].y;
+  }
+  return new Vector(x, y);
+};
+
+const sub = (p, ...rest) => {
+  let { x, y } = p;
+  for (let i = 0; i < rest.length; i++) {
+    x -= rest[i].x;
+    y -= rest[i].y;
+  }
+  return new Vector(x, y);
+};
+
+const distance = (a, b) =>
+  Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+
+const multiplyScalar = (p, v) => {
+  p.x *= v;
+  p.y *= v;
+  return p;
+};
+
+const magnitude = p => distance(p, { x: 0, y: 0 });
+
+const unitVector = (p, limit = 1) => {
+  const m = magnitude(p) || 1;
+  return new Vector(p.x * limit / m, p.y * limit / m);
+};
+
 class Vector {
   constructor(x, y) {
     this.x = x || 0;
     this.y = y || 0;
   }
 
-  add(v2) {
-    let x = this.x;
-    let y = this.y;
-    for (let i = 0; i < arguments.length; i++) {
-      x += arguments[i].x;
-      y += arguments[i].y;
-    }
-    return new Vector(x, y);
+  add() {
+    return add(this, ...arguments);
   }
 
-  sub(v2) {
-    let x = this.x;
-    let y = this.y;
-    for (let i = 0; i < arguments.length; i++) {
-      x -= arguments[i].x;
-      y -= arguments[i].y;
-    }
-    return new Vector(x, y);
+  sub() {
+    return sub(this, ...arguments);
   }
 
-  distance(v2) {
-    return Math.sqrt(Math.pow(this.x - v2.x, 2) + Math.pow(this.y - v2.y, 2));
+  distance(p) {
+    return distance(this, p);
   }
 
-  multiplyScalar(val) {
-    this.x *= val;
-    this.y *= val;
-    return this;
+  multiplyScalar(v) {
+    return multiplyScalar(this, v);
   }
 
   magnitude() {
-    return this.distance(this.origin);
+    return magnitude(this);
   }
 
   unitVector(limit = 1) {
-    const m = this.magnitude() || 1;
-    return new Vector(this.x * limit / m, this.y * limit / m);
+    return unitVector(this, limit);
   }
 }
 
