5cb7efb3c2fe9ea104bbbfe40c5e18b2e2c8a08e
Charles Pinnix <cpinnix89@gmail.com>
2018-10-30 08:44:37 -0400

    Tree Refactoring

--- src/components/vs-flock/quadtree.js
+++ src/components/vs-flock/quadtree.js
@@ -1,7 +1,123 @@
 import equals from "./point/equals";
 import contains from "./box/contains";
-import split from "./box/split";
+import bSplit from "./box/split";
 import overlaps from "./box/overlaps";
+
+const insert = (tree, point, object) => {
+  //check if should contain point
+  if (!contains(tree.box.low, tree.box.high, point)) {
+    return tree;
+  }
+
+  //if is a leaf node and not full, then insert
+  //need to check if it already exists though
+  let i;
+  if (
+    tree.children === null &&
+    (tree.value.length < tree.max || tree.level === tree.max_level)
+  ) {
+    for (i = 0; i < tree.value.length; i++) {
+      if (equals(tree.value[i].point, point)) {
+        tree.value[i].value = object;
+        return;
+      }
+    }
+    tree.value.push({ point, value: object });
+    return tree;
+  }
+
+  //if is a leaf node but full, call split
+  if (tree.children === null && tree.level < tree.max_level) {
+    tree.split();
+  }
+
+  // if is not a leaf node, call insert on child nodes
+  for (i = 0; i < tree.children.length; i++) {
+    tree.children[i].insert(point, object);
+  }
+  tree.value = [];
+
+  return tree;
+};
+
+const split = tree => {
+  //split into 4 congruent child quadrants using box quadrant method
+  tree.nextLevel = tree.level + 1;
+  tree.children = bSplit(tree.box.low, tree.box.high);
+
+  for (var i = 0; i < tree.children.length; i++) {
+    tree.children[i] = new Quadtree(
+      tree.children[i],
+      tree.max,
+      tree.max_level,
+      tree.nextLevel
+    );
+  }
+
+  //redistribute values to appropriate child nodes
+  for (i = 0; i < tree.value.length; i++) {
+    for (let k = 0; k < tree.children.length; k++) {
+      tree.children[k].insert(tree.value[i].point, tree.value[i].value);
+    }
+  }
+};
+
+const queryRangeRec = (tree, box, result) => {
+  //if query area doesn't overlap this box then return
+  if (!overlaps(tree.box, box)) {
+    return;
+  }
+  //if leaf node with contained value(s), then check against contained objects
+  let i;
+  if (tree.value.length > 0) {
+    for (i = 0; i < tree.value.length; i++) {
+      if (contains(box.low, box.high, tree.value[i].point)) {
+        result.push(tree.value[i]);
+      }
+    }
+    return;
+  }
+  //if has children, then make recursive call on children
+  if (tree.children !== null) {
+    for (i = 0; i < tree.children.length; i++) {
+      tree.children[i]._queryRangeRec(box, result);
+    }
+    return;
+  }
+};
+
+const queryRange = (tree, box, result) => {
+  return queryRangeRec(tree, box, result);
+};
+
+const queryPoint = (tree, point) => {
+  //return value if point in tree
+  if (!contains(tree.box.low, tree.box.high, point)) {
+    return null;
+  }
+
+  if (tree.value.length > 0) {
+    for (var i = 0; i < tree.value.length; i++) {
+      if (equals(tree.value[i].point, point)) {
+        return tree.value[i].value;
+      }
+    }
+  }
+
+  if (tree.children !== null) {
+    let val = null;
+    for (var i = 0; i < tree.children.length; i++) {
+      val = val || tree.children[i].queryPoint(point);
+    }
+    return val;
+  }
+  return null;
+};
+
+const clear = tree => {
+  tree.children = null;
+  tree.value = [];
+};
 
 export default class Quadtree {
   constructor(box, max, max_level, level) {
@@ -14,118 +130,28 @@ export default class Quadtree {
   }
 
   insert(point, object) {
-    //check if should contain point
-    if (!contains(this.box.low, this.box.high, point)) {
-      return this;
-    }
-
-    //if is a leaf node and not full, then insert
-    //need to check if it already exists though
-    let i;
-    if (
-      this.children === null &&
-      (this.value.length < this.max || this.level === this.max_level)
-    ) {
-      for (i = 0; i < this.value.length; i++) {
-        if (equals(this.value[i].point, point)) {
-          this.value[i].value = object;
-          return;
-        }
-      }
-      this.value.push({ point, value: object });
-      return this;
-    }
-
-    //if is a leaf node but full, call split
-    if (this.children === null && this.level < this.max_level) {
-      this.split();
-    }
-
-    // if is not a leaf node, call insert on child nodes
-    for (i = 0; i < this.children.length; i++) {
-      this.children[i].insert(point, object);
-    }
-    this.value = [];
-    return this;
+    return insert(this, point, object);
   }
 
   split() {
-    //split into 4 congruent child quadrants using box quadrant method
-    this.nextLevel = this.level + 1;
-    this.children = split(this.box.low, this.box.high);
-    for (var i = 0; i < this.children.length; i++) {
-      this.children[i] = new Quadtree(
-        this.children[i],
-        this.max,
-        this.max_level,
-        this.nextLevel
-      );
-    }
-    //redistribute values to appropriate child nodes
-    for (i = 0; i < this.value.length; i++) {
-      for (let k = 0; k < this.children.length; k++) {
-        this.children[k].insert(this.value[i].point, this.value[i].value);
-      }
-    }
+    split(this);
   }
 
   queryRange(box) {
-    //return all point/value pairs contained in range
     const result = [];
-    this._queryRangeRec(box, result);
+    queryRange(this, box, result);
     return result;
   }
 
   _queryRangeRec(box, result) {
-    //if query area doesn't overlap this box then return
-    if (!overlaps(this.box, box)) {
-      return;
-    }
-    //if leaf node with contained value(s), then check against contained objects
-    let i;
-    if (this.value.length > 0) {
-      for (i = 0; i < this.value.length; i++) {
-        if (contains(box.low, box.high, this.value[i].point)) {
-          result.push(this.value[i]);
-        }
-      }
-      return;
-    }
-    //if has children, then make recursive call on children
-    if (this.children !== null) {
-      for (i = 0; i < this.children.length; i++) {
-        this.children[i]._queryRangeRec(box, result);
-      }
-      return;
-    }
+    return queryRangeRec(this, box, result);
   }
 
   queryPoint(point) {
-    //return value if point in tree
-    if (!this.box.contains(point)) {
-      return null;
-    }
-
-    if (this.value.length > 0) {
-      for (var i = 0; i < this.value.length; i++) {
-        if (equals(this.value[i].point, point)) {
-          return this.value[i].value;
-        }
-      }
-    }
-
-    if (this.children !== null) {
-      let val = null;
-      for (var i = 0; i < this.children.length; i++) {
-        val = val || this.children[i].queryPoint(point);
-      }
-      return val;
-    }
-    return null;
+    return queryPoint(this, point);
   }
 
   clear() {
-    this.children = null;
-    this.value = [];
+    clear(this);
   }
 }
